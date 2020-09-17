66aedf755a002177baf8b2368636a5f1dd869fd4
Karl Baumgartner <seegeekrun@users.noreply.github.com>
2020-09-17 11:13:00 -0500

    Delete backend.tf

--- staging/backend.tf
+++
@@ -1,8 +0,0 @@
-
-terraform {
-backend "gcs" {
-  credentials = "deep-dive-dataflow.json"
-  bucket = "static-state-289320"   # GCS bucket name to store terraform tfstate
-  prefix = "deep-dive"           # Update to desired prefix name. Prefix name should be unique for each Terraform project having same remote state bucket.
-  }
-}
